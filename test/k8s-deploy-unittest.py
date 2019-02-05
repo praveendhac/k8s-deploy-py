a11e28072b4922aae8462777934ce705883753a6
Praveen Darshanam <pdarshanam@ptc.com>
2019-02-05 00:22:28 +0000

    adding test cases

--- test/k8s-deploy-unittest.py
+++ test/k8s-deploy-unittest.py
@@ -1,3 +1,45 @@
+#!/usr/bin/env python3
+
+import sys
+sys.path.append('../src')
+import src.k8s-deploy
+
+try:
+  import kubernetes as k8s
+  from kubernetes.client.rest import ApiException
+except ImportError:
+  print("kubernetes package not installed, run \"pip3 install kubernetes\"")
+  sys.exit(-1)
+
+try:
+  import pytest
+except ImportError:
+  print("kubernetes package not installed, run \"pip3 install pytest\"")
+  sys.exit(-1)
+
+def setClusterContext(kubeconfig):
+  '''Loads authentication and cluster information from kube-config file'''
+  if kubeconfig:
+    click.echo('Using kubeconfig file: %s' % kubeconfig)
+    k8s.config.load_kube_config(config_file=kubeconfig)
+  else:
+    click.echo('Using kubeconfig file from default location')
+    k8s.config.load_kube_config(config_file=None)
+
+def initializeCoreV1Api(kubeconfig):
+  setClusterContext(kubeconfig)
+  corev1api = k8s.client.CoreV1Api()
+  return corev1api
+
+def test_get_namespaces():
+  # set kubeconfig context using export KUBECONFIG for now
+  kubeconfig = ""
+  corev1api = initializeCoreV1Api(kubeconfig)
+  try: 
+    api_response = api_instance.list_namespace(include_uninitialized=include_uninitialized, pretty=pretty, _continue=_continue, field_selector=field_selector, label_selector=label_selector, limit=limit, resource_version=resource_version, timeout_seconds=timeout_seconds, watch=watch)
+  except ApiException as e:
+    print("Exception when calling CoreV1Api->list_namespace: %s\n" % e)
+
 api_server()
   '''successfully get API server'''
 
