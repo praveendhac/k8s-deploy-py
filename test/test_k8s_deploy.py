ac254a09ab108f2fc1bd901eee0fb7a9851a5fac
Praveen Darshanam <pdarshanam@ptc.com>
2019-02-06 23:54:36 +0000

    updated test cases

--- test/test_k8s_deploy.py
+++ test/test_k8s_deploy.py
@@ -25,71 +25,97 @@ def getCoreV1Api():
   corev1api = k8s.client.CoreV1Api()
   return corev1api 
 
-#def setClusterContext(kubeconfig):
-#  '''Loads authentication and cluster information from kube-config file'''
-#  if kubeconfig:
-#    click.echo('Using kubeconfig file: %s' % kubeconfig)
-#    k8s.config.load_kube_config(config_file=kubeconfig)
-#  else:
-#    click.echo('Using kubeconfig file from default location')
-#    k8s.config.load_kube_config(config_file=None)
-#
-#def initializeCoreV1Api(kubeconfig):
-#  setClusterContext(kubeconfig)
-#  corev1api = k8s.client.CoreV1Api()
-#  return corev1api
+def getCoreApi():
+  k8s.config.load_kube_config()
+  coreapi = k8s.client.CoreApi()
+  return coreapi
+
+def getRbacAuthorizationApi():
+  k8s.config.load_kube_config()
+  rbacauthorizationapi = k8s.client.RbacAuthorizationV1Api()
+  return rbacauthorizationapi
 
 def test_get_all_namespaces():
+  ns = []
   corev1api = getCoreV1Api()
   print("Testcase: get_all_namespaces")
   try: 
     api_response = corev1api.list_namespace()
   except ApiException as e:
     print("Exception when calling CoreV1Api->list_namespace: %s\n" % e)
-  print("ns:", api_response)
-
-test_get_all_namespaces()
+  resp_dict = api_response.to_dict()
+  for item in resp_dict['items']:
+    metadata = item['metadata']
+    # add all the namespaces in the cluster to ns list
+    ns.append(metadata['name'])
+  print("ns:", ns)
+  assert len(ns) >= 3
 
 def test_api_server():
   '''successfully get API server'''
   print("Testcase: api_server")
-  corev1api = getCoreV1Api()
-  print(__name__)
-test_api_server()
+  coreapi = getCoreApi()
+  resp = coreapi.get_api_versions().to_dict()
+  # Kubernetes API Server IP:Port or domain:port
+  api_server = resp['server_address_by_client_cid_rs'][0]['server_address']
+  # checks api_server is not empty
+  assert api_server
 
-def test_api_v1():
+def test_api_v1_resources():
   '''basic check'''
   print("Testcase: api_v1")
   corev1api = getCoreV1Api()
   try:
-    api_resp = corev1api.get_api_resources()
+    api_resp = corev1api.get_api_resources().to_dict()
   except ApiException as e:
     print("Exception when calling CoreV1Api->get_api_resources: %s\n" % e)
-  print(__name__, "api_v1:", api_resp)
-
-test_api_v1()
+  # totak k8s resources for v1 API
+  total_k8s_v1_api_resources = len(api_resp['resources'])
+  print("Total k8s v1_api resources:", total_k8s_v1_api_resources)
+  assert total_k8s_v1_api_resources > 1
   
 def test_is_rbac_enabled():
   '''RBAC check'''
   print("Testcase: is_rbac_enabled")
-  corev1api = getCoreV1Api()
+  rbacauthorizationapi = getRbacAuthorizationApi()
+  #resp = rbacauthorizationapi.get_api_group().to_dict()
+  resp = rbacauthorizationapi.get_api_resources().to_dict()
+  print("resp:", resp)
+test_is_rbac_enabled()
 
 def test_check_nodes():
   '''check node'''
   print("Testcase: check_nodes")
   corev1api = getCoreV1Api()
   try:
-    ret = corev1api.list_node()
-    print(ret)
+    ret = corev1api.list_node().to_dict()
   except ApiException as e:
     print("Exception when calling CoreV1Api->list_node: %s\n" % e)
-
+  # check atleast one node is present
+  first_node = ret['items'][0]['metadata']['name']
+  assert first_node
 test_check_nodes()
 
-def test_check_node_pressures():
+def test_check_node_health():
   '''check node pressure'''
+  node_status = ''
   print("Testcase: check_node_pressures")
   corev1api = getCoreV1Api()
+  corev1api = getCoreV1Api()
+  try:
+    ret = corev1api.list_node().to_dict()
+  except ApiException as e:
+    print("Exception when calling CoreV1Api->list_node: %s\n" % e)
+  # check atleast one node is present
+  print(">>status:",ret['items'][0]['status']['conditions'][0])
+  for conditions in ret['items'][0]['status']['conditions']:
+    print("conditions['type']", conditions['type'])
+    if conditions['type'] == 'Ready':
+      node_status = conditions['status']
+  print("node_status:",node_status)
+  # checking the first node to be healthy
+  # TODO: check health of all nodes
+  assert node_status == 'True'
 
 def test_check_control_plane():
   '''all control plane pods'''
@@ -106,10 +132,9 @@ def test_check_pods():
   print("Testcase: check_pods")
   corev1api = getCoreV1Api()
   ret = corev1api.list_pod_for_all_namespaces(watch=False)
+  print("ret:", ret)
   for i in ret.items:
     print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
-
-test_check_pods()
 
 def test_check_deployments():
   '''check deployments'''
@@ -120,13 +145,15 @@ def test_check_services():
   '''check services'''
   print("Testcase: check_services")
   corev1api = getCoreV1Api()
-  list_service_for_all_namespaces()
+  resp = corev1api.list_service_for_all_namespaces()
+  print("resp:", resp)
 
 def test_check_secrets():
   '''check secrets'''
   print("Testcase: check_secrets")
   corev1api = getCoreV1Api()
-  list_secret_for_all_namespaces()
+  resp = corev1api.list_secret_for_all_namespaces()
+  print("resp:", resp)
 
 def test_check_service_rechability():
   '''port-forward'''
@@ -137,8 +164,13 @@ def test_detect_nonrunning_pods():
   '''pods not running'''
   print("Testcase: detect_nonrunning_pods")
   corev1api = getCoreV1Api()
+  ret = corev1api.list_pod_for_all_namespaces(watch=False)
+  print("ret:", ret)
+  for i in ret.items:
+    print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
 
 def test_check_configmap_allns():
   print("Testcase: check_configmap_allns")
   corev1api = getCoreV1Api()
-  list_config_map_for_all_namespaces()
+  resp = corev1api.list_config_map_for_all_namespaces()
+  print("resp:", resp)
